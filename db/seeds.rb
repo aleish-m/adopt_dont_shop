f022d38a7571b541245e249b745f96fc265cd77f
Erik Riggs <riggs.erik@gmail.com>
2022-09-04 17:04:36 -0600

    Heroku1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 Shelter.destroy_all
 # Pet.destroy_all
-Applicant.destroy_all
+# Applicant.destroy_all
 
 @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
 @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
