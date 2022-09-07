9eb43015dfd8bc98c41e7ce4c986394de3956160
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-06 23:34:09 -0600

    wrote tests for corrected app_status and approved? methods in applicant model

--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -22,4 +22,28 @@ RSpec.describe Applicant, type: :model do
       in_array(['In Progress', 'Pending', 'Approved', 'Rejected'])
     end
   end
+
+  describe 'class methods' do
+    before :each do
+    @shelter = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+    @john = Applicant.create!(first_name: 'John', last_name: 'Dough', street_address: '123 Fake Street', city: 'Denver', state: 'CO', zip: 80205, description: "I'm awesome", status: 'Pending')
+    @pet_1 = @john.pets.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
+    @pet_2 = @john.pets.create!(name: 'Jake', age: 5, breed: 'Pug', adoptable: true, shelter_id: @shelter.id)
+    @applicant = ApplicantPet.find_by(applicant_id: @john.id, pet_id: @pet_1.id)
+    @applicant_2 = ApplicantPet.find_by(applicant_id: @john.id, pet_id: @pet_2.id)
+    @applicant.update(approved: true)
+    end
+
+    it "#self.approved?" do
+      expect(@john.approved?(@pet_1.id)).to eq true
+      expect(@john.approved?(@pet_2.id)).to be (nil)
+    end
+
+    it "#self.app_status" do
+      expect(@john.app_status(@pet_1.id)).to have_attributes ({:id => @applicant.id, :applicant_id => @john.id, :approved => true, :pet_id => @pet_1.id})
+      expect(@john.app_status(@pet_2.id)).to be_an ApplicantPet
+      expect(@john.app_status(@pet_1.id)).to eq(@applicant)
+      expect(@john.app_status(@pet_1.id)).to_not eq(@applicant_2)
+    end
+  end
 end
